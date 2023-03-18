08bb25a5ca9b989f8f32034994ac472c9451ccd3
mahaswetha2004 <mahaswetha.selvaraj@gmail.com>
2023-03-18 12:21:44 +0530

    sql  queries

--- mysql-scripts.sql
+++ mysql-scripts.sql
@@ -1,1 +1,6 @@
 create table employee1(empid varchar(10) primary key,empname char(20),mailid char(20));
+
+desc employee1;
+
+insert into employee1(empid,empname,mailid) values('011a','aki','aki@gmail.com');
+select*from employee1;
