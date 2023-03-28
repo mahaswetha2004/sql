ad34ed234065cb446be50973336a2bd50364f852
mahaswetha2004 <mahaswetha.selvaraj@gmail.com>
2023-03-28 19:16:45 +0530

    sql queries

--- mysql-scripts.sql
+++ mysql-scripts.sql
@@ -6,3 +6,11 @@ insert into employee1(empid,empname,mailid) values('011a','aki','aki@gmail.com')
 select*from employee1;
 
 create table student(id varchar(10),name char(20));
+create table sales_order(sorder varchar(10),product_no varchar(10),qty varchar(10));
+insert into sales_order(sorder,product_no,qty) values('p01','a1',10);
+insert into sales_order(sorder,product_no,qty) values('p01','a2',12);
+insert into sales_order(sorder,product_no,qty) values('p01','a3',10);
+insert into sales_order(sorder,product_no,qty) values('p02','a1',8);
+insert into sales_order(sorder,product_no,qty) values('p02','a2',32);
+insert into sales_order(sorder,product_no,qty) values('p02','a3',10);
+select product_no,qty from sales_order groupby product_no;
